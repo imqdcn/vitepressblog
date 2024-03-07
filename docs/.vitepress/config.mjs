a8cb8d02a0c0432e69966e9c979f9ef340ce7939
ycluo <imqdcn@gmail.com>
2024-03-07 21:10:31 +0800

    add deploy

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
+    // base: '/vitepressblog/',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
