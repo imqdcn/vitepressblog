e7b881b1c73cbf42846c0e4c475e21d0a3ace51f
ycluo <imqdcn@gmail.com>
2024-03-07 21:07:16 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '指南', link: '/guide/',activeMatch: '/guide/' } //指南
+    ],
+
+    sidebar: {
+      "/":
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ]
+      },
+      "/guide/":{
+        // text: '指南',
+        items: [
+          { 
+            text: '指南', 
+            items:
+            [
+              { text: '测试页面', link: '/guide/测试' },
+              { text: '测试页面2', link: '/guide/测试2' }
+            ],
+            collapsed: false,
+          },
+          { 
+            text: '写作', 
+            items:
+            [
+              { text: '测试页面3', link: '/guide/测试3' },
+              { text: '测试页面4', link: '/guide/测试4' }
+            ],
+            collapsed: true,
+          },
+        ]
+      },
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
